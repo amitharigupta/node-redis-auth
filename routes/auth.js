5b478785fc7ac364939c77406fa9a62a945c98b8
amitharigupta <amitgt9967@gmail.com>
2022-12-26 20:30:57 +0530

    changes done for login and register routes

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
 
+const tempDB = [];
+
 /* GET auth listing. */
-router.post('/login', function(req, res, next) {
-  res.send('respond with a resource');
+// Register Route
+router.post('/register', async (req, res, next) => {
+  const { username, password } = req.body;
+  let existingUser = tempDB.find((user) => user.username === username);
+  if(existingUser) {
+    return res.statusCode(422).json({ status: 422, message: "User is already exist" })
+  }
+  const user = { username, password };
+  tempDB.push(user);
+  req.session.user = user;
+
+  res.statusCode(200).json({ status: 200, message: "User created successfully", data: user });
 });
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
 var express = require('express');
+const path = require('path');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+const publicRootConfig = { 
+  root: path.join(__dirname, '../public')
+}
+
+router.get('/login', function(req, res, next) {
+  res.sendFile('login.html', publicRootConfig);
+});
+
+router.get('/register', function(req, res, next) {
+  res.sendFile('register.html', publicRootConfig);
 });
 
 module.exports = router;
